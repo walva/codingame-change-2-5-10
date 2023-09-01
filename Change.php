74413fb45431802e14c75e3726a1331b693f2e1c
Ellis Benjamin <benjamin.ellis@goldenpalace.be>
2023-09-01 18:06:03 +0200

    use POO & recurcive approach

--- Change.php
+++ Change.php
@@ -2,11 +2,49 @@
 
 class Change
 {
-    public int $coin2 = 0;
-    public int $bill5 = 0;
-    public int $bill10 = 0;
+    public $coin2 = 0;
+    public $bill5 = 0;
+    public $bill10 = 0;
+
+    public function add($coinOrBill){
+        switch($coinOrBill){
+            case 2: $this->coin2 += 1; break;
+            case 5 : $this->bill5 += 1; break;
+            case 10 : $this->bill10 += 1; break;
+        }
+    }
+
+    public function computeRest($amount){
+        return $amount
+        - $this->coin2 * 2
+        - $this->bill5 * 5
+        - $this->bill10 * 10;
+    }
+    public function getTotal(){
+        return $this->coin2 * 2
+        + $this->bill5 * 5
+        + $this->bill10 * 10;
+    }
+
+    public static function computeOptimalChange(Change $change = null, $amount, $toAdd = null)
+    {
+        $change = $change??new Change();
+        if ($change->getTotal() > $amount) {
+            return null;
+        }
+        if ($change->computeRest($amount) == 0) {
+            return $change;
+        }
+    
+        foreach([10,5,2] as $coin){
+            $candidate = (clone $change);
+            $candidate->add($coin);
+            $result = self::computeOptimalChange($candidate, $amount, $coin);
+            if($result != null) return $result;
+        }
+        return null;
+    }
+
 }
-
-
 
 
--- optimalChange.php
+++ optimalChange.php
@@ -2,33 +2,7 @@
 
 require_once('Change.php');
 
-function optimalChange(int $required_change): Change|null
+function optimalChange($s): Change|null
 {
-    if ($required_change <= 1 || $required_change === 3) {
-        return null;
-    }
-
-    $change = new Change();
-
-    $leftover_change = $required_change;
-
-    $change->bill10 = intdiv($leftover_change, 10);
-    $leftover_change %= 10;
-
-    if ($leftover_change === 1 || $leftover_change === 3) {
-        $change->bill10--;
-        $leftover_change += 10;
-    }
-
-    $change->bill5 = intdiv($leftover_change, 5);
-    $leftover_change %= 5;
-
-    if ($leftover_change === 1 || $leftover_change === 3) {
-        $change->bill5--;
-        $leftover_change += 5;
-    }
-
-    $change->coin2 = intdiv($leftover_change, 2);
-
-    return $change;
+    return Change::computeOptimalChange(null, $s);
 }
